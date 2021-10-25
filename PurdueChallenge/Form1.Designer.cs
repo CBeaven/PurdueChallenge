f962ae576e7c753e5686637b8609ea3106c28dcf
CBeaven <cmbeaven03@gmail.com>
2021-10-25 08:07:37 -0500

    Add project files.

---
+++ PurdueChallenge/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace PurdueChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblFactorial = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(173, 87);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Magic";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblFactorial
+            // 
+            this.lblFactorial.AutoSize = true;
+            this.lblFactorial.Location = new System.Drawing.Point(147, 123);
+            this.lblFactorial.Name = "lblFactorial";
+            this.lblFactorial.Size = new System.Drawing.Size(47, 13);
+            this.lblFactorial.TabIndex = 1;
+            this.lblFactorial.Text = "Factorial";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(150, 153);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(83, 13);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "Sum of Factorial";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 233);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.lblFactorial);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Who Cares What This Is Named";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblFactorial;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ PurdueChallenge/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PurdueChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        long n = 20;
+        int m = 20;
+        long s = 0;
+        int a = 0;
+        string t;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            for (int i = m - 1; i > 0; i--)
+            {
+                n *= i;
+            }
+            string t = n.ToString();
+            lblFactorial.Text = t;
+            s = 0;
+            while (n != 0)
+            {
+                s += n % 10;
+                n /= 10;
+            }
+            lblSum.Text = s.ToString();
+        }
+    }
+}
